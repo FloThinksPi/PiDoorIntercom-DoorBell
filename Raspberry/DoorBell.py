5c81254e2d287ed376007f6840becbdb70692242
Florian Braun <FloThinksPi@users.noreply.github.com>
2018-01-07 12:14:31 +0100

    Code cleanup
    there was some really ugly stuff here

--- Raspberry/DoorBell.py
+++ Raspberry/DoorBell.py
@@ -5,64 +5,39 @@ import requests
 from multiprocessing.dummy import Pool
 from time import sleep
 
+# Pin setup
 btnPin = 14
 ledPin = 15
+# Button blinking settings
+numBlink = 8 # Number of ON-OFF loops
+blinkTime = 0.3 # Time(seconds) to sleep between each gpio state switch
+#Gong URL
+gongURL = 'http://YOURIP/gong'
 
+# Misc
 pool = Pool(1)
-
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(btnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Fix , remove pullup
+GPIO.setup(btnPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(ledPin, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.add_event_detect(btnPin, GPIO.FALLING, bouncetime=1000)
 
 def btn_pushed(channel):
-    print 'PUSHED!'
-
     pool.apply_async(gong())
-
     blink()
 
 def gong():
-    requests.get('http://10.110.109.118/gong')
+    requests.get(gongURL)
     return 0
 
 def blink():
 
-    GPIO.output(ledPin, GPIO.LOW)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.HIGH)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.LOW)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.HIGH)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.LOW)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.HIGH)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.LOW)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.HIGH)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.LOW)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.HIGH)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.LOW)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.HIGH)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.LOW)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.HIGH)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.LOW)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.HIGH)
-    sleep(0.25)
-    GPIO.output(ledPin, GPIO.LOW)
-    sleep(0.25)
-    GPIO.output(15, GPIO.HIGH)
+    for x in range(numBlink):
+        GPIO.output(ledPin, GPIO.LOW)
+        sleep(blinkTime)
+        GPIO.output(ledPin, GPIO.HIGH)
+        sleep(blinkTime)
+
+    GPIO.output(ledPin, GPIO.HIGH) # Be high again at the end
     return 0
 
 GPIO.add_event_callback(btnPin, btn_pushed)
